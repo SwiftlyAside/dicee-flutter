2327c38c408cbc672c20bdb38ec9afaedc2e0c02
Swiftly <ivecis@live.co.kr>
2021-04-17 23:26:22 +0900

    Modify main.dart dice face randomized

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.deepPurple,
+        backgroundColor: Color(0xfff2acda),
         appBar: AppBar(
           title: const Text('Dicee'),
-          backgroundColor: Colors.deepPurple,
+          backgroundColor: Color(0xfff2acda),
         ),
         body: DicePage(),
       ),
@@ -15,29 +17,43 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 3;
     return Center(
       child: Row(
         children: [
           Expanded(
             child: TextButton(
                 onPressed: () {
-                  print('Left button got pressed.');
+                  changeDiceFace();
                 },
                 child: Image.asset('images/dice$leftDiceNumber.png')),
           ),
           Expanded(
             child: TextButton(
                 onPressed: () {
-                  print('Right button got pressed.');
+                  changeDiceFace();
                 },
-                child: Image.asset('images/dice2.png')),
+                child: Image.asset('images/dice$rightDiceNumber.png')),
           ),
         ],
       ),
     );
+  }
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 }
